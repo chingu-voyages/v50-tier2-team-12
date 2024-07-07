1763210431325915343b94a7d6ff096dbc60de5b
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-07-07 14:52:23 +0200

    Setup vite and stuff

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">"Chingu Voyage - Best Team"</h1>
+    </>
+  );
+}
+
+export default App;
