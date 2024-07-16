fdd9f500a4ad3a58f6550450b28aff7783ce25f2
Efezino Idisi <enduranceidisi13@gmail.com>
2024-07-16 16:12:09 +0100

    add error page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,15 @@
 import { Route } from 'react-router-dom';
 import App from '../App';
+import Error from '../pages/error/Error';
 import Home from '../pages/home/Home';
 
 const routes = (
   <Route path='/' element={<App />}>
     <Route index element={<Home />} />
+
+    {/* catch all route to handle unknown routes*/}
+    {/* all other routes should be above */}
+    <Route path='*' element={<Error />} />
   </Route>
 );
 
