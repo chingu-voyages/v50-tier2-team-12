25180dbc7393ed76806333dfa238b41c5a976bc5
Efezino Idisi <enduranceidisi13@gmail.com>
2024-07-22 10:21:56 +0100

    add remove order button

--- src/components/order/Order.jsx
+++ src/components/order/Order.jsx
@@ -1,3 +1,6 @@
+import { useOrder } from '../../contexts/OrderContext';
+import { Icons } from '../Icons';
+
 export default function OrderList({ orders }) {
   if (!orders || orders.length === 0) {
     return (
@@ -15,13 +18,35 @@ export default function OrderList({ orders }) {
   );
 }
 
-function OrderItem({ name, price, dsc }) {
+function OrderItem({ name, price, dsc, quantity, id }) {
+  const [_, dispatchOrder] = useOrder();
+
+  const removeOrder = (id) => {
+    if (!id) return;
+
+    dispatchOrder({
+      type: 'REMOVE_ORDER',
+      payload: id,
+    });
+  };
   return (
     <article className=''>
-      <h3 className='font-medium text-xl'>{name}</h3>
+      <div className='flex justify-between items-center mb-2'>
+        <h3 className='font-medium text-xl'>{name}</h3>
+        <button
+          aria-label='remove from order'
+          className='group'
+          onClick={() => removeOrder(id)}
+        >
+          <Icons.trash className='group-hover:active-link group-active:active-link' />
+        </button>
+      </div>
       <div className='flex justify-between items-end'>
         <p className='max-w-48 text-grey'>{dsc}</p>
-        <p className='font-medium text-xl'>${price}</p>
+        <p className='font-medium text-xl space-x-3'>
+          <span>x{quantity}</span>
+          <span>${price}</span>
+        </p>
       </div>
     </article>
   );
