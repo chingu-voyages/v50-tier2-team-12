3bd8feea97a61ef3421f6fe325256a6438a0a7da
Efezino Idisi <enduranceidisi13@gmail.com>
2024-07-22 20:27:11 +0100

    loader

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,8 @@
+import { useRouteLoaderData } from 'react-router-dom';
+
 export default function HomePage() {
+  const menus = useRouteLoaderData('menu');
+  console.log(menus);
   return (
     <>
       <h2>homepage content here</h2>
