5a9d792589e70805c04b1617c1db220c557035b9
Mary Richelle <mary.richal44@gmail.com>
2024-07-29 19:30:15 +0300

    indentation

--- src/components/homepage/Restaurant.jsx
+++ src/components/homepage/Restaurant.jsx
@@ -8,21 +8,21 @@ const Restaurant = ({ restaurant }) => {
   return (
     <section className="leading-5 py-6" >
       <Link to={`/restaurants/${restaurant.name}`}>
-      <LazyLoad
-        height={200}
-        offset={100}
-        placeholder={<div className="h-28 w-96 sm:w-80 bg-disabled animate-pulse rounded-t-2xl mb-3"></div>}
-      >
-        <img
-          src={restaurant.img}
-          loading="lazy"
-          onError={handleError}
-          className="h-28 w-96 sm:w-80 object-cover rounded-t-2xl bg-gradient-to-r from-gray-200 to-gray-500 mb-3"
-          alt={restaurant.name}
-        />
-      </LazyLoad>
-      <h2 className="text-lg font-semibold">{restaurant.name}</h2>
-      <p>{restaurant.country}</p>
+        <LazyLoad
+          height={200}
+          offset={100}
+          placeholder={<div className="h-28 w-96 sm:w-80 bg-disabled animate-pulse rounded-t-2xl mb-3"></div>}
+        >
+          <img
+            src={restaurant.img}
+            loading="lazy"
+            onError={handleError}
+            className="h-28 w-96 sm:w-80 object-cover rounded-t-2xl bg-gradient-to-r from-gray-200 to-gray-500 mb-3"
+            alt={restaurant.name}
+          />
+        </LazyLoad>
+        <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+        <p>{restaurant.country}</p>
       </Link>
     </section>
   );
