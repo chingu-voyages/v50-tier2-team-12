c66f54c2b6538e227e1f48e22e9817b4500a6185
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-08-01 17:57:57 +0200

    Save and load credits from local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import MobileNavbar from './components/navigation/MobileNavbar';
 import { Footer } from './components/Footer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [credits, setCredits] = useState(40);
+  const [credits, setCredits] = useState(() => {
+    // Load credits from local storage or initialize to $40 if no storage exists yet
+    const storedCredits = localStorage.getItem('credits');
+    return storedCredits ? JSON.parse(storedCredits) : 40;
+  });
   const [isCredits, setIsCredits] = useState(false);
+
+  // Side effect to update local storage every time credit changes
+  useEffect(() => {
+    localStorage.setItem('credits', JSON.stringify(credits));
+  }, [credits]);
 
   return (
     <>
