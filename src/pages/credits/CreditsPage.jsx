a5c1951ede6474567308e6e167c98d51bc9dee89
Efezino Idisi <enduranceidisi13@gmail.com>
2024-08-11 21:34:40 +0100

    remove testing border

--- src/pages/credits/CreditsPage.jsx
+++ src/pages/credits/CreditsPage.jsx
@@ -3,8 +3,8 @@ import PageHeading from '../../components/headings/PageHeading';
 
 export default function CreditsPage() {
   return (
-    <div className='md:bg-light-grey border border-light-grey min-h-[calc(100lvh-12rem)] md:flex md:flex-col md:justify-center md:items-center'>
-      <div className='bg-white md:rounded-3xl md:max-w-[32.25rem] md:mx-auto w-full md:shadow-card-shadow md:p-10 relative'>
+    <div className='md:bg-light-grey min-h-[calc(100lvh-12rem)] md:flex md:flex-col md:justify-center md:items-center'>
+      <div className='md:bg-white md:rounded-3xl md:max-w-[516px] md:mx-auto w-full md:shadow-card-shadow md:p-10 relative md:min-h-[655px] md:my-5'>
         <PageHeading title={'credits'} className='md:p-0' />
         <Credits />
       </div>
